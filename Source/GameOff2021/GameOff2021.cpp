78c4e91ff33ebafc0cb7b3f13fe6d5cac31a28d5
Petrickah <36566884+Petrickah@users.noreply.github.com>
2021-11-02 17:52:13 +0200

    Added project files

---
+++ Source/GameOff2021/GameOff2021.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameOff2021.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameOff2021, "GameOff2021" );

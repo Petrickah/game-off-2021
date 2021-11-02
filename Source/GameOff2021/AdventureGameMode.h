37c2f4de33bb672e3569f0258abf8f697ae31eb9
Petrickah <36566884+Petrickah@users.noreply.github.com>
2021-11-02 18:27:03 +0200

    Added AdventureGameMode class file

---
+++ Source/GameOff2021/AdventureGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdventureGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEOFF2021_API AAdventureGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

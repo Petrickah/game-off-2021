6808aa957028ad86c54860a9fcc960cf1e707aaf
Petrickah <36566884+Petrickah@users.noreply.github.com>
2021-11-12 13:26:15 +0200

    Implemented spells

---
+++ Source/GameOff2021/MagicalStaff.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CindyCharacter.h"
+#include "SpellType.h"
+#include "MagicalStaff.generated.h"
+
+UCLASS()
+class GAMEOFF2021_API AMagicalStaff : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMagicalStaff();
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	class USkeletalMeshComponent* StaffMesh;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	bool IsStaffEquiped = false;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	bool IsAttacking = false;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	bool IsCasting = false;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void DestroyEffect(ACindyCharacter* StaffOwner);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StopMontage(UAnimMontage* Montage, bool Interrupted);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void SetStaffVisibility(bool Visibility);
+
+	UFUNCTION(BlueprintCallable)
+	void ToggleStaff(ACindyCharacter* StaffOwner);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void EquipStaff(ACindyCharacter* StaffOwner);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void ShowMagic(ACindyCharacter* StaffOwner, FTransform RelativeTransform);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void EquippingStaff(ACindyCharacter* StaffOwner);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Attack(ACindyCharacter* StaffOwner, UAnimMontage* AnimMontage, bool IsPrimary = true);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void CastSpell(ACindyCharacter* StaffOwner, ESpellType SpellType);
+
+	UFUNCTION(BlueprintPure)
+	FTransform GetRelativeTransform(class USkeletalMeshComponent* SKMesh, class USpringArmComponent* CameraBoom);
+};
